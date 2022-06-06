43aa2c38db820a48ea1b5166cdad4115b6fbfe61
Lu-Smith <artistlunasmith@gmail.com>
2022-06-06 19:01:24 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello World</div>
+      <button className="btn btn-primary">Hello World</button>
+    </div>
+  );
+}
+
+export default App;
