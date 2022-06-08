e910533a9172b799f68d2eede6530c9b137b35db
Lu-Smith <artistlunasmith@gmail.com>
2022-06-08 10:39:15 +0100

    Added photos and photos api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,17 +2,30 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results.js";
+import Photos from "./Photos.js";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.initialKeyword);
   let [searchResult, setSearchResult] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function search() {
     // documentation: https://dictionaryapi.dev/
     let searchWord = keyword;
     let urlApi = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`;
     axios.get(urlApi).then(handleResponse);
+    let pexelsApiKey = `563492ad6f91700001000001ecfdf91227964c65855c59628b5f476e`;
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios
+      .get(pexelsUrl, {
+        headers: headers,
+      })
+      .then(handlePexelsResponse);
   }
   function load() {
     setLoaded(true);
@@ -44,6 +57,9 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results data={searchResult} />
+        <div className="row">
+          <Photos photos={photos} />
+        </div>
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row photo">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div key={index} className="col-4">
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={photo.src.landscape}
+                    alt={photo.alt}
+                    className="img-fluid"
+                  />
+                </a>
+                <p>
+                  photographer:{" "}
+                  <a
+                    href={photo.photographer_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="photographer"
+                  >
+                    {photo.photographer}
+                  </a>
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
